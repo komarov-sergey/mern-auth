406af35802c0fcf369d724cb14e392cc46cec4da
Sergey <komarovs33@mail.ru>
2020-08-29 21:29:27 +0300

    add .env

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
-
-const authRoutes = require("./routes/auth");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const authRoutes = require("./routes/auth");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
 app.use("/api", authRoutes);
 
 const port = process.env.port || 8000;
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port} env = ${process.env.NODE_ENV}`);
 });
