b6d88c1047de1a43c117ad56e7b4908c76a29e63
Sergey <komarovs33@mail.ru>
2020-09-17 12:24:22 +0300

    add active links

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, match, history }) => {
+  const isActive = (path) => {
+    if (history.location.pathname === path) {
+      return { color: "#000" };
+    } else {
+      return { color: "#fff" };
+    }
+  };
+
   const nav = () => (
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link to="/" className="text-light nav-link">
+        <Link to="/" className="nav-link" style={isActive("/")}>
           Home
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/signin" className="text-light nav-link">
+        <Link to="/signin" className="nav-link" style={isActive("/signin")}>
           Signin
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/signup" className="text-light nav-link">
+        <Link to="/signup" className="nav-link" style={isActive("/signup")}>
           Signup
         </Link>
       </li>
@@ -29,4 +37,4 @@ const Layout = ({ children }) => {
   );
 };
 
-export default Layout;
+export default withRouter(Layout);
