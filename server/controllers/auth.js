7870fb9136aa5e931feeb0c5a2d95c5a2eb748ee
Sergey <komarovs33@mail.ru>
2020-09-18 18:36:43 +0300

    add reset forgot password email

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -142,3 +142,57 @@ exports.adminMiddleware = (req, res, next) => {
     next();
   });
 };
+
+exports.forgotPassword = (req, res) => {
+  const { email } = req.body;
+
+  User.findOne({ email }, async (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: "User with that email does not exist" });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_RESET_PASSWORD, {
+      expiresIn: "10m",
+    });
+
+    try {
+      const request = await mailjet.post("send", { version: "v3.1" }).request({
+        Messages: [
+          {
+            From: {
+              Email: process.env.EMAIL_FROM,
+              Name: "Sergey",
+            },
+            To: [
+              {
+                Email: email,
+                Name: "Sergey",
+              },
+            ],
+            Subject: "Password reset link",
+            HTMLPart: `
+            <h1>Please use the following link to reset your password</h1>
+            <p>${process.env.CLIENT_URL}/auth/password/reset/${token}</p>
+            <hr />
+            <p>This email may contain sensetive information</p>
+            <p>${process.env.CLIENT_URL}</p>
+            `,
+            CustomID: "AppGettingStartedTest",
+          },
+        ],
+      });
+      return res.json({
+        message: `Email has been sent to ${email}. Follow the instraction to activate your account.`,
+      });
+    } catch (e) {
+      console.log({ e });
+      return res.json({
+        message: e.message,
+      });
+    }
+  });
+};
+
+exports.resetPassword = (req, res) => {};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const { signup, accountActivation, signin } = require("../controllers/auth");
+const {
+  signup,
+  accountActivation,
+  signin,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/auth");
 const {
   userSignupValidator,
   userSigninValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator,
 } = require("../validators/auth");
 const { runValidation } = require("../validators");
 
@@ -12,5 +20,19 @@ const router = express.Router();
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/account-activation", accountActivation);
 router.post("/signin", userSigninValidator, runValidation, signin);
+
+//forgot reset password
+router.put(
+  "/forgot-password",
+  forgotPasswordValidator,
+  runValidation,
+  forgotPassword
+);
+router.post(
+  "/reset-password",
+  resetPasswordValidator,
+  runValidation,
+  resetPassword
+);
 
 module.exports = router;
